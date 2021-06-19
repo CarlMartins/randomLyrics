17a5303538fa0b10b8b8ff74cbc68f8eb9845769
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-19 12:16:23 -0300

    Busca por todas as musicas com letras na API

--- backend/src/controllers/indexController.js
+++ backend/src/controllers/indexController.js
@@ -1,5 +1,30 @@
+const baseUrl = require('../services/api');
+const axios = require('axios').default;
+
+
 exports.Song = (req, res) => {
-  res.send({
-    mensagem: "Ola mundo"
-  });
-}
+  getSongData()
+    .then((value) => {
+      res.send(value);
+    })
+    .catch((err) => res.send(err));
+};
+
+const getSongData = async () => {
+  let songData = {};
+  try {
+    do {
+      let artistId = rndInt();
+      songData = await axios.get(`${baseUrl.baseUrl}track.search?s_track_rating=desc&f_has_lyrics=1&page_size=100&f_artist_id=${artistId}&apikey=${baseUrl.apikey}`);
+      console.log(songData.data.message.header.available);
+    } while (songData.data.message.header.available == 0);
+  } catch (error) {
+    return "Falha";
+  }
+  return songData.data;
+};
+
+function rndInt() {
+  let result = Math.floor(Math.random() * 300000) + 1;
+  return result;
+};
---
+++ backend/src/services/api.js
@@ -0,0 +1,6 @@
+const api = {
+  apikey: '4f81a3066ca06bac4a95816bb378378b',
+  baseUrl: 'https://api.musixmatch.com/ws/1.1/'
+};
+
+module.exports = api;
